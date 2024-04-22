68a89310cdb9dc05ecf9811e5cf1ff5c927ffe27
namazso <admin@namazso.eu>
2024-04-22 23:10:05 +0200

    Add UNIX socket support

--- src/main.c
+++ src/main.c
@@ -1,49 +1,95 @@
 #include <arpa/inet.h>
 #include <pthread.h>
 #include <sys/socket.h>
+#include <sys/un.h>
 #include <unistd.h>
 
 #include <errno.h>
 #include <stdint.h>
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 #include "http.h"
 
+static void usage(const char* name) {
+  fprintf(stderr, "Usage: %s <port>|unix:<socket>", name);
+}
+
 int main(int argc, char* argv[]) {
-  long lport;
-  if (argc < 2 || (lport = atol(argv[1])) <= 0 || lport > 0xffff) {
-    errno = EINVAL;
-    perror(NULL);
+  if (argc != 2) {
+    usage(argv[0]);
     exit(EXIT_FAILURE);
   }
 
-  const uint16_t port = lport;
+  int server_fd;
 
-  struct sockaddr_in server_addr;
+  if (0 == strncmp(argv[1], "unix:", 5)) {
+    const char* path = argv[1] + 5;
+    const int length = strlen(path);
+    if (length == 0 || length >= 108) {
+      errno = EINVAL;
+      perror(NULL);
+      exit(EXIT_FAILURE);
+    }
 
-  // create server socket
-  const int server_fd = socket(AF_INET, SOCK_STREAM, 0);
-  if (server_fd < 0) {
-    perror("socket failed");
-    exit(EXIT_FAILURE);
-  }
+    // create server socket
+    server_fd = socket(AF_UNIX, SOCK_STREAM, 0);
+    if (server_fd < 0) {
+      perror("socket failed");
+      exit(EXIT_FAILURE);
+    }
 
-  int reuseaddr = 1;
-  if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuseaddr, sizeof(reuseaddr))) {
-    perror("setsockopt failed");
-    exit(EXIT_FAILURE);
-  }
+    int reuseaddr = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuseaddr, sizeof(reuseaddr))) {
+      perror("setsockopt failed");
+      exit(EXIT_FAILURE);
+    }
 
-  // config socket
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_addr.s_addr = INADDR_ANY;
-  server_addr.sin_port = htons(port);
+    // config socket
+    struct sockaddr_un server_addr;
+    server_addr.sun_family = AF_UNIX;
+    strcpy(server_addr.sun_path, path);
 
-  // bind socket to port
-  if (bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
-    perror("bind failed");
-    exit(EXIT_FAILURE);
+    // bind socket to port
+    if (bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+      perror("bind failed");
+      exit(EXIT_FAILURE);
+    }
+  } else {
+    long lport;
+    if ((lport = atol(argv[1])) <= 0 || lport > 0xffff) {
+      errno = EINVAL;
+      perror(NULL);
+      exit(EXIT_FAILURE);
+    }
+
+    const uint16_t port = lport;
+
+    // create server socket
+    server_fd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_fd < 0) {
+      perror("socket failed");
+      exit(EXIT_FAILURE);
+    }
+
+    int reuseaddr = 1;
+    if (setsockopt(server_fd, SOL_SOCKET, SO_REUSEADDR, &reuseaddr, sizeof(reuseaddr))) {
+      perror("setsockopt failed");
+      exit(EXIT_FAILURE);
+    }
+
+    // config socket
+    struct sockaddr_in server_addr;
+    server_addr.sin_family = AF_INET;
+    server_addr.sin_addr.s_addr = INADDR_ANY;
+    server_addr.sin_port = htons(port);
+
+    // bind socket to port
+    if (bind(server_fd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0) {
+      perror("bind failed");
+      exit(EXIT_FAILURE);
+    }
   }
 
   // listen for connections
@@ -52,7 +98,7 @@ int main(int argc, char* argv[]) {
     exit(EXIT_FAILURE);
   }
 
-  printf("server listening on port %u\n", (unsigned)port);
+  printf("server listening on port %s\n", argv[1]);
   while (1) {
     // client info
     struct sockaddr_in client_addr;
